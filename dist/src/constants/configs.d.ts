4b723d2fcf0d7eddce9638d58b691f6e8dd56b9b
SangNX <130561684+camewell071@users.noreply.github.com>
2023-05-30 12:30:20 +0700

    chore: update pubsub url

--- dist/src/constants/configs.d.ts
+++ dist/src/constants/configs.d.ts
@@ -1,2 +1,2 @@
-export declare const BASE_URL = "https://wadary.trustless.computer/relayer";
+export declare const BASE_URL = "https://wadary.regtest.trustless.computer/relayer";
 export declare const WALLET_URL = "https://newbitcoinwallet.io/";
